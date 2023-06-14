9b83c3d5242226cb02f1682c2418172e28394079
Rumanmoulik <rumanmoulik@gmail.com>
2023-06-14 08:19:43 +0530

    adaboost code updates

--- Codes/adaboost.py
+++ Codes/adaboost.py
@@ -12,7 +12,7 @@ with open("pca_20.dat") as f:
     
 dim = len(data[0])-1
     
-av=np.zeros([dim])
+time=np.zeros([dim])
 maxv=np.zeros([dim])
 
 print("AdaBoosting with pca data") 
@@ -20,36 +20,51 @@ X=np.array([data[i][1:] for i in range(len(data))])
 Y = np.array([int(data[i][0]) for i in range(len(data))]).reshape([len(data)])
 
 for i in range(1,dim+1):
-    clf = AdaBoostClassifier(n_estimators=trees, learning_rate=lr)
-    cvf = cross_validate(clf,X[:,:i],Y,cv=5)
-    #print(round(clf.score(X_test,Y_test),5),end=' ')
-    print(cvf)
-    av[i-1]=sum(cvf['test_score'])/len(cvf['test_score'])
-    maxv[i-1]=max(cvf['test_score'])
+    print("dimension: ",i)
+    for j in range(0,10):
+        clf = AdaBoostClassifier(n_estimators=trees, learning_rate=lr)
+        cvf = cross_validate(clf,X[:,:i],Y,cv=5,n_jobs=5)
+        #print(round(clf.score(X_test,Y_test),5),end=' ')
+        print(cvf)
+        time[i-1]+=sum(cvf['fit_time'])+sum(cvf['score_time'])
+        maxv[i-1]+=max(cvf['test_score'])
 
+raw_time=0
+raw_maxv=0
 with open("data.dat") as f:
     data = [line.strip().split() for line in f]
-
 
 print("AdaBoosting with raw data")    
 X = np.array([data[i][1:-3] for i in range(1,len(data))])
 Y = np.array([int(data[i][-1]) for i in range(1,len(data))]).reshape([len(data)-1])
-clf=AdaBoostClassifier(n_estimators=trees, learning_rate=lr)
-cvf = cross_validate(clf,X,Y,cv=5)
-print(cvf)
+for i in range(0,10):
+    clf=AdaBoostClassifier(n_estimators=trees, learning_rate=lr)
+    cvf = cross_validate(clf,X,Y,cv=5,n_jobs=5)
+    print(cvf)
+    raw_time+=sum(cvf['fit_time'])+sum(cvf['score_time'])
+    raw_maxv+=max(cvf['test_score'])
 
-print("average value Summary")
-print("PCA data",av)
-Average = sum(cvf['test_score'])/len(cvf['test_score'])
-print("raw_data",Average)
+print("\nMaximum value Summary")
+print("PCA data",maxv/10)
+print("raw_data",raw_maxv/10)
 
-print("maximum value Summary")
-print("PCA data",maxv)
-print("raw_data",max(cvf['test_score']))
+print("\nTime Summary")
+print("PCA data",time/10)
+print("raw_data",raw_time/10)
 
 with open("adaboost_"+str(trees)+"_"+str(int(lr*100))+".dat","w") as f:
-    line = str(max(cvf['test_score']))
+    f.write("Maximum score summary\n")
+    f.write("raw data: "+str(raw_maxv/10)+"\n")
+    line=""
     for i in maxv:
-        line+=" "+str(i)
+        line+=" "+str(i/10)
+    line+="\n"
+    f.write(line)
+    
+    f.write("Time summary\n")
+    f.write("raw data: "+str(raw_time/10)+"\n")
+    line=""
+    for i in time:
+        line+=" "+str(i/10)
     line+="\n"
     f.write(line)
