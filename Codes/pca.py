9d57c74a2048396802e3449c90e37612a5ba18e0
Rumanmoulik <rumanmoulik@gmail.com>
2023-06-15 08:16:35 +0530

    Update pca.py
    Now plots all 3 types of kernels
    also writes the pca data from rbf kernel

--- Codes/pca.py
+++ Codes/pca.py
@@ -12,48 +12,38 @@ X=np.array([data[i][1:-3] for i in range(1,len(data))])
 samples, features = np.shape(X)
 Y = np.array([int(data[i][-1]) for i in range(1,len(data))]).reshape([len(data)-1])
 
-
+#Power Scaler Transformer
 X_train=X
 Y_train=Y
-#scaler = preprocessing.StandardScaler()
 scaler = preprocessing.PowerTransformer()
 scaler.fit(X_train)
-#print(scaler.mean_)
 X_train_trans = scaler.transform(X_train)
-#X_test_trans = scaler.transform(X_test)
 
-#pca = PCA()
-pca = KernelPCA(kernel='rbf',n_components=20)
-
+#Linear Kernel PCA
+pca = KernelPCA(kernel='linear',n_components=20)
 pca.fit(X_train_trans,Y_train)
 pca_train = pca.transform(X_train_trans)
-#pca_test = pca.transform(X_test_trans)
-#print(pca_result)
-#per_var = np.round(pca.explained_variance_ratio_*100, decimals=1)
-#print(per_var)
+plt.figure()
+plt.scatter(pca_train[:, 0], pca_train[:, 1], c=Y_train)
+plt.savefig("pca/pca_linear.png")
 
-'''
-_, (train_ax, test_ax) = plt.subplots(ncols=2, sharex=True, sharey=True, figsize=(8, 4))
-train_ax.scatter(pca_train[:, 0], pca_train[:, 1], c=Y_train)
-#test_ax.scatter(pca_test[:, 0], pca_test[:, 1], c=Y_test)
-plt.show()
-'''
+#polynomial Kernel PCA
+pca = KernelPCA(kernel='polynomial',n_components=20)
+pca.fit(X_train_trans,Y_train)
+pca_train = pca.transform(X_train_trans)
+plt.figure()
+plt.scatter(pca_train[:, 0], pca_train[:, 1], c=Y_train)
+plt.savefig("pca/pca_polynomial.png")
 
-fig=plt.figure()
-ax=fig.add_subplot(1,1,1)
-ax.scatter(pca_train[:, 0], pca_train[:, 1], c=Y_train)
-#ax=fig.add_subplot(1,2,2)
-#ax.scatter(pca_test[:, 0], pca_test[:, 1], c=Y_test)
-plt.savefig("pca_2d.png")
 
-'''
-fig=plt.figure()
-ax=fig.add_subplot(1,1,1,projection="3d")
-ax.scatter3D(pca_train[:, 0], pca_train[:, 1], pca_train[:, 2], c=Y_train)
-#ax=fig.add_subplot(1,2,2,projection="3d")
-#ax.scatter3D(pca_test[:, 0], pca_test[:, 1], pca_test[:, 2], c=Y_test)
-plt.show()
-'''
+#rbf Kernel PCA
+pca = KernelPCA(kernel='rbf',n_components=20)
+pca.fit(X_train_trans,Y_train)
+pca_train = pca.transform(X_train_trans)
+plt.figure()
+plt.scatter(pca_train[:, 0], pca_train[:, 1], c=Y_train)
+plt.savefig("pca/pca_rbf.png")
+
 with open("pca_20.dat","w") as f:
     for i in range(len(X)):
         line=str(Y[i])
