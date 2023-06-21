5d195dec8993824ee92e6c2aab193cc24861da7b
Rumanmoulik <rumanmoulik@gmail.com>
2023-06-21 19:09:02 +0530

    Update pca.py
    small bugfix
    added pickling

--- Codes/pca.py
+++ Codes/pca.py
@@ -3,6 +3,7 @@ from sklearn.decomposition import KernelPCA, PCA
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
+from pickle import dump
 
 with open("data.dat") as f:
     data = [line.strip().split() for line in f]
@@ -28,7 +29,7 @@ plt.scatter(pca_train[:, 0], pca_train[:, 1], c=Y_train)
 plt.savefig("pca/pca_linear.png")
 
 #polynomial Kernel PCA
-pca = KernelPCA(kernel='polynomial',n_components=20)
+pca = KernelPCA(kernel='poly',n_components=20)
 pca.fit(X_train_trans,Y_train)
 pca_train = pca.transform(X_train_trans)
 plt.figure()
@@ -51,3 +52,6 @@ with open("pca_20.dat","w") as f:
             line+=" "+str(round(pca_train[i][j],5))
         line+="\n"
         f.write(line)
+
+dump(scaler, open("trained_scaler.pkl",'wb'))
+dump(pca, open("trained_pca.pkl",'wb'))
