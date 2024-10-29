37dab18d1bca42cfd7e262835087c3949b00cf76
Ruman Moulik <rumanmoulik@gmail.com>
2024-10-29 15:20:56 +0530

    Adding more analysis

--- adaboost_time.py
+++ adaboost_time.py
@@ -4,15 +4,10 @@ graph_titles = ["SG:62; $N_s$=12","SG:139; $N_s$=5","SG:189; $N_s$=9","SG:221; $
 
 file_names = ["grp_62-site_12","grp_139-site_5","grp_189-site_9","grp_221-site_2","grp_221-site_5","grp_225-site_2"]
 
-fig_maxv,ax_maxv = plt.subplots(2,3, figsize=(6.5,3.5), sharex=True,sharey=True)
+fig_maxv,ax_maxv = plt.subplots(2,3, figsize=(6.5,3.5), sharex=True)
 dim_x = [i for i in range(1,21)]
 fig_maxv.subplots_adjust(hspace=0.1,wspace=0.1)
-ax_maxv[0,0].axvline(x = 6, color = 'grey', linestyle = '--')
-ax_maxv[1,0].axvline(x = 4, color = 'grey', linestyle = '--')
-ax_maxv[0,1].axvline(x = 4, color = 'grey', linestyle = '--')
-ax_maxv[1,1].axvline(x = 6, color = 'grey', linestyle = '--')
-ax_maxv[0,2].axvline(x = 3, color = 'grey', linestyle = '--')
-ax_maxv[1,2].axvline(x = 3, color = 'grey', linestyle = '--')
+
 
 
 for i in range(0,6):
@@ -21,11 +16,11 @@ for i in range(0,6):
 
     with open(file_names[i]+"/adaboost/adaboost_2500_2.dat") as f:
         data = [line.strip().split() for line in f]
-        raw_maxv = float(data[1][2])
-        maxv = [float(num) for num in data[2]]
+        raw_time1 = float(data[4][2])
+        time1 = [float(num) for num in data[5]]
     
-    ax_maxv[row,col].axhline(y = raw_maxv, color = 'red', linestyle = '--')        
-    ax_maxv[row,col].plot(dim_x,maxv, marker = '.',color="c")
+    ax_maxv[row,col].axhline(y = raw_time1, color = 'red', linestyle = '--')        
+    ax_maxv[row,col].plot(dim_x,time1, marker = '.',color="c")
     ax_maxv[row,col].set_title(graph_titles[i],x=0.5,y=0.01,fontsize=8)
     
 
@@ -36,8 +31,9 @@ ax_maxv[1,0].tick_params(axis='both', which='major', labelsize=8)
 ax_maxv[1,1].tick_params(axis='both', which='major', labelsize=8)
 ax_maxv[1,2].tick_params(axis='both', which='major', labelsize=8)
 fig_maxv.tight_layout()
-fig_maxv.savefig("adaboost_max_score.pdf", format='pdf')
+fig_maxv.savefig("adaboost_times.pdf", format='pdf')
 
+'''
 with open("grp_221-site_2/adaboost/adaboost_2500_2.dat") as f:
     data = [line.strip().split() for line in f]
     raw_time1 = float(data[4][2])
@@ -48,18 +44,17 @@ with open("grp_221-site_5/adaboost/adaboost_2500_2.dat") as f:
     time2 = [float(num) for num in data[5]]
     
 
-fig_time = plt.figure(figsize=[4,4])
+fig_time = plt.figure(figsize=[4,3.5])
 plt.plot(dim_x,time1, marker = 'o', color="blue", label=graph_titles[3])
 plt.plot(dim_x,time2, marker = 's', color="orange", label=graph_titles[4])
 plt.axhline(y = raw_time1, linestyle = '--', color="c", label=graph_titles[3]+" raw data")
 plt.axhline(y = raw_time2, linestyle = ':', color="red", label=graph_titles[4]+" raw data")
-plt.xticks([1,5,10,15,20],[1,5,10,15,20],fontsize=10)
-plt.tick_params(axis='both', which='major', labelsize=10)
-plt.xlabel("No. of dimensions",fontsize=10)
-plt.ylabel("Time (s)",fontsize=10)
-plt.legend(fontsize=8.7, loc="lower right")
+plt.xticks([1,5,10,15,20],[1,5,10,15,20],fontsize=8)
+plt.tick_params(axis='both', which='major', labelsize=8)
+plt.xlabel("No. of dimensions",fontsize=8)
+plt.ylabel("Time (s)",fontsize=8)
+plt.legend(fontsize=6, loc="lower right")
 plt.tight_layout()
 fig_time.savefig("adaboost_time.pdf", format='pdf')
 #plt.show()
-    
-    
+'''
---
+++ grp_62-site_12/isolation/mlp.py
@@ -0,0 +1,61 @@
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import cross_validate
+import pickle
+import sys
+from dotenv import dotenv_values
+import os
+import random
+import timeit
+
+params=dotenv_values("params")
+dim = int(params['dim'])
+iters = int(params['iters'])
+lr = float(params['lr'])
+alpha = float(params['alpha'])
+hl_size = int(params['hl_size'])
+hl_depth = int(params['hl_depth'])
+hl=np.full(hl_depth,hl_size)
+#seeds = random.sample(range(0,100000), 1000)
+best_score = 0
+with open("pca_20.dat") as f:
+    data = [line.strip().split() for line in f]
+start_time = 0
+for i in range(10):
+    print(start_time)
+    print("New Model Start------------------------------------------")
+    print("Iterations: ",iters)
+    print("Learning Rate: ",lr)
+    print("Alpha: ",alpha)
+    print("HL Depth: ",hl_depth)
+    print("HL Size: ",hl_size)
+    #print("Random Seed: ",i)
+
+    print("MLP with pca data") 
+    X=np.array([data[i][1:dim+1] for i in range(len(data))],dtype=float)
+    Y = np.array([int(data[i][0]) for i in range(len(data))],dtype=int).reshape([len(data)])
+
+    clf = MLPClassifier(max_iter=iters, learning_rate_init=lr, alpha=alpha, hidden_layer_sizes=hl)
+    cvf = cross_validate(clf,X,Y,cv=5,return_estimator=True, n_jobs=5)
+    print(cvf['fit_time'])
+    print(cvf['test_score'])
+
+    print("maximum value Summary")
+    max_ind = np.argmax(cvf['test_score'])
+    print(max_ind, max(cvf['test_score']))
+
+    clf_best = cvf['estimator'][max_ind]
+    score = clf_best.score(X,Y)
+    print("score on whole dataset: ",score)
+
+    if score > best_score:
+        best_score=score
+        print("Saving this model")
+        pickle.dump(clf_best, open("trained_mlp.pkl", 'wb'))
+        
+    print("-----------------------------------\n")
+        
+    print("Highest score attained: ", best_score)
+    #print("Time taken: ", timeit.default_timer()-start_time)
+    start_time+=(sum(cvf['fit_time'])+sum(cvf['score_time']))
+print(start_time/10)
